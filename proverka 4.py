0a44053741229e2286252d7545f3420fc5f2b313
Dmitrykob04 <113111874+Dmitrykob04@users.noreply.github.com>
2022-12-21 23:17:24 +0300

    Add files via upload

---
+++ proverka 4.py
@@ -0,0 +1,15 @@
+import figures.circle as a
+import figures.triangle as b
+import figures.square as c
+a1 = a.circle_perimeter(7)
+a2 = a.circle_area(3)
+b1 = b.triangle_perimeter(5, 6, 7)
+b2 = b.triangle_area(2,5,6)
+c1 = c.square_perimeter(9)
+c2 = c.square_area(4)
+print(a1)
+print(a2)
+print(b1)
+print(b2)
+print(c1)
+print(c2)
