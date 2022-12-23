eb6448fa1a52c16c28e595c5af5d4bfd7480de1f
Dmitrykob04 <113111874+Dmitrykob04@users.noreply.github.com>
2022-12-23 21:38:07 +0300

    Add files via upload

---
+++ proverka 4.py
@@ -0,0 +1,23 @@
+from figures import circle_area
+from figures import circle_perimeter
+from figures import triangle_perimeter
+from figures import triangle_area
+from figures import square_perimeter
+from figures import square_area
+circle_area()
+circle_area(7)
+
+circle_perimeter()
+circle_perimeter(9)
+
+triangle_perimeter()
+triangle_perimeter(4,3,7)
+
+triangle_area()
+triangle_area(1,9,6)
+
+square_perimeter()
+square_perimeter(2)
+
+square_area()
+square_area(7)
